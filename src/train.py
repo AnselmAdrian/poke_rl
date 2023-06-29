f1a9b24769bba460b39086a4d78be20beef8b560
AnselmAdrian <81840345+AnselmAdrian@users.noreply.github.com>
2023-06-29 09:25:18 +0800

    model training and tuning files

---
+++ src/train.py
@@ -0,0 +1,110 @@
+import numpy as np
+from gym.spaces import Space, Box
+from poke_env.player import Gen8EnvSinglePlayer
+from datetime import datetime
+import torch as th
+
+from gym.utils.env_checker import check_env
+from poke_env.player import RandomPlayer
+from stable_baselines3 import PPO
+import time
+import asyncio
+from poke_env.player import background_evaluate_player
+from common import SimpleRLEnv, RLEnv, evaluate_player, TestRLPlayer
+from common.model import CustomMLP
+
+if __name__ == '__main__':
+
+    async def main(player1, player2):
+        start = time.time()
+
+        # Now, let's evaluate our player
+        await player1.battle_against(player2, n_battles=100)
+
+        print(
+            "Max damage player won %d / 100 battles [this took %f seconds]"
+            % (
+                player1.n_won_battles, time.time() - start
+            )
+        )
+
+    opponent = RandomPlayer(battle_format="gen8randombattle")
+    #train_env = SimpleRLEnv(battle_format="gen8randombattle", opponent = opponent, start_challenging=True)
+    train_env = RLEnv(battle_format="gen8randombattle", opponent = opponent, start_challenging=True)
+    try:
+        print('Checking Environment')
+        check_env(train_env)
+    except Exception as e:
+        print(e)
+
+    # Create stable baseline model
+    # kwargs = {
+    #             'learning_rate': 0.0003071888865904524, 
+    #             'gamma': 0.3511201957562162, 
+    #             'gae_lambda': 0.5924822869558098, 
+    #             'normalize_advantage': True, 
+    #             'ent_coef': 1.726829000128019e-07, 
+    #             'vf_coef': 0.17962332078088222
+    #             }
+    kwargs = {
+                'learning_rate': 0.0001,  
+                'gamma': 0.95, 
+                'gae_lambda': 0.5924822869558098, 
+                'normalize_advantage': True, 
+                'ent_coef': 1.7e-07, 
+                'vf_coef': 0.18
+                }
+    
+    policy_kwargs = dict(activation_fn=th.nn.ReLU,
+                            net_arch=dict(pi=[32, 32], vf=[32, 32]),
+                            features_extractor_class=CustomMLP,
+                            features_extractor_kwargs=dict(features_dim=128)
+                    )
+
+    model = PPO('MlpPolicy', env = train_env, verbose = 1, batch_size = 64, 
+                policy_kwargs = policy_kwargs, **kwargs, tensorboard_log="logs/ppo/")
+    model.load('data/06_model/pokemon/TestRLEnv/ppo_20230625_081359.h5') # Best version so far
+    try:
+        model.learn(500_000)
+    except Exception as e:
+        print('Stopping training early due to error!')
+        print(e)
+    except KeyboardInterrupt:
+        print('Stopping training early')
+
+    #Save model
+    date_now = datetime.now().strftime('%Y%m%d_%H%M%S')
+    filepath = f'data/06_model/pokemon/TestRLEnv/ppo_{date_now}.h5'
+    print('saving file at:', filepath)
+    model.save(filepath)
+
+    train_env.close()
+
+    # train_env = SimpleRLPlayer(
+    #     battle_format="gen8randombattle", opponent=opponent, start_challenging=True
+    # )
+    opponent = lambda: RandomPlayer(battle_format="gen8randombattle")
+    rl_player = lambda: TestRLPlayer(model = model,
+        battle_format="gen8randombattle",
+    )
+    evaluate_player(rl_player, opponent)
+
+    # n_challenges = 100
+    # placement_battles = 40
+    # eval_task = background_evaluate_player(
+    #     eval_env.agent, n_challenges, placement_battles
+    # )   
+
+    # obs = eval_env.reset()
+    # for _ in range(100):
+    #     action, _states = model.predict(obs)
+    #     obs, rewards, dones, info = eval_env.step(action)
+    #     if dones:
+    #         print('Done')
+    #         break
+    #     eval_env.render()
+    # eval_env.close()
+
+
+    #print("Evaluation with included method:", eval_task.result())
+    #asyncio.get_event_loop().run_until_complete(main())
